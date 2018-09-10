2b31b7a8d8aff3c76bdb072b7313959223b697a6
DayoWang <dayowang@hotmail.com>
2018-09-10 21:26:07 +0800

    格式化代码

--- src/main/java/producer/BlockChain.java
+++ src/main/java/producer/BlockChain.java
@@ -12,82 +12,83 @@ import java.util.ArrayList;
  * @date 2018/9/4
  */
 public class BlockChain {
-    private static ArrayList<Block> blocklist = new ArrayList<>();
 
-    /**
-     * 挖矿复杂度
-     */
-    private static int difficulty = 20;
+  private static ArrayList<Block> blocklist = new ArrayList<>();
 
-    /**
-     * 生成创世块
-     */
-    public static Block creatGenesisBlock() {
-        return new Block(0, "进行coinbase交易，生成第一个交易输出", "0");
+  /**
+   * 挖矿复杂度
+   */
+  private static int difficulty = 20;
+
+  /**
+   * 生成创世块
+   */
+  private static Block creatGenesisBlock() {
+    return new Block(0, "进行coinbase交易，生成第一个交易输出", "0");
+  }
+
+  /**
+   * 创建新区快
+   */
+  private static Block newBlock(int index, String data, String previousHash) {
+    return new Block(index, data, previousHash);
+  }
+
+  /**
+   * 获取区块链中最新区块
+   */
+  private static Block getLatestBlock() {
+    return blocklist.get(blocklist.size() - 1);
+  }
+
+  /**
+   * 添加区块
+   */
+  private static void addBlock(Block block) {
+    //通过矿工们来做工作量证明（POW）来挖掘并验证新的区块
+    block.ProofOfWork(difficulty);
+    //添加区块
+    blocklist.add(block);
+  }
+
+  /**
+   * 检查链的完整性
+   */
+  private static Boolean isChainValid() {
+    Block currentBlock;
+    Block previousBlock;
+
+    //循环遍历每个块检查hash,从1开始，0为创世块（数据固定）
+    for (int i = 1; i < blocklist.size(); i++) {
+      currentBlock = blocklist.get(i);
+      previousBlock = blocklist.get(i - 1);
+      //判断链中当前块的hash变量的值与计算出来的hash是否相等
+      if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
+        System.out.println("Current Hashes not equal");
+        return false;
+      }
+      //判断previousHash变量的值与前一个块的hash是否相等
+      if (!previousBlock.hash.equals(currentBlock.previousHash)) {
+        System.out.println("Previous Hashes not equal");
+        return false;
+      }
     }
+    return true;
+  }
 
-    /**
-     * 创建新区快
-     */
-    public static Block newBlock(int index, String data, String previousHash) {
-        return new Block(index, data, previousHash);
-    }
+  public static void main(String[] args) {
+    System.out.println("正在尝试挖掘创世块... ");
+    addBlock(creatGenesisBlock());
 
-    /**
-     * 获取区块链中最新区块
-     */
-    public static Block getLatestBlock() {
-        return blocklist.get(blocklist.size() - 1);
-    }
+    System.out.println("正在尝试挖掘第2个区块... ");
+    addBlock(newBlock(1, "这是第二个区块", getLatestBlock().hash));
 
-    /**
-     * 添加区块
-     */
-    public static void addBlock(Block block) {
-        //通过矿工们来做工作量证明（POW）来挖掘并验证新的区块
-        block.ProofOfWork(difficulty);
-        //添加区块
-        blocklist.add(block);
-    }
+    System.out.println("正在尝试挖掘第3个区块... ");
+    addBlock(newBlock(2, "这是第三个区块", getLatestBlock().hash));
 
-    /**
-     * 检查链的完整性
-     */
-    private static Boolean isChainValid() {
-        Block currentBlock;
-        Block previousBlock;
+    System.out.println("\n检查链的完整性: " + isChainValid());
 
-        //循环遍历每个块检查hash,从1开始，0为创世块（数据固定）
-        for (int i = 1; i < blocklist.size(); i++) {
-            currentBlock = blocklist.get(i);
-            previousBlock = blocklist.get(i - 1);
-            //判断链中当前块的hash变量的值与计算出来的hash是否相等
-            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
-                System.out.println("Current Hashes not equal");
-                return false;
-            }
-            //判断previousHash变量的值与前一个块的hash是否相等
-            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
-                System.out.println("Previous Hashes not equal");
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("正在尝试挖掘创世块... ");
-        addBlock(creatGenesisBlock());
-
-        System.out.println("正在尝试挖掘第2个区块... ");
-        addBlock(newBlock(1, "这是第二个区块", getLatestBlock().hash));
-
-        System.out.println("正在尝试挖掘第3个区块... ");
-        addBlock(newBlock(2, "这是第三个区块", getLatestBlock().hash));
-
-        System.out.println("\n检查链的完整性: " + isChainValid());
-
-        System.out.println("\n区块链如下: ");
-        System.out.println(StringTool.getJson(blocklist));
-    }
+    System.out.println("\n区块链如下: ");
+    System.out.println(StringTool.getJson(blocklist));
+  }
 }
